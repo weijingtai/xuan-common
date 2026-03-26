3a4a6003ab29893854f0bf9ca277b52f902fecf9
wjt <wjt19951215@gmail.com>
2026-03-25 22:54:56 -0700

    Bridge four zhu theme export surfaces to card package

--- lib/features/four_zhu_card/four_zhu_card.dart
+++ lib/features/four_zhu_card/four_zhu_card.dart
@@ -2,6 +2,6 @@ library;
 
 /// Compatibility facade for the Four Zhu Card render/UI surface during the
 /// package extraction transition.
-export '../../themes/editable_four_zhu_card_theme.dart';
+export 'package:xuan_four_zhu_card/themes/editable_four_zhu_card_theme.dart';
 export '../../widgets/editable_fourzhu_card.dart';
 export '../../widgets/four_zhu_add_palette.dart';
