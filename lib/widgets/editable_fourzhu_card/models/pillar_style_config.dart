33ee5ea659d5002521f3f76730737faaf16ca441
wjt <wjt19951215@gmail.com>
2026-03-25 22:49:17 -0700

    Bridge four zhu style model facades to card package

--- lib/widgets/editable_fourzhu_card/models/pillar_style_config.dart
+++ lib/widgets/editable_fourzhu_card/models/pillar_style_config.dart
@@ -1,2 +1,5 @@
-export '../../../features/four_zhu_card/widgets/editable_fourzhu_card/models/pillar_style_config.dart';
+library;
 
+/// Compatibility facade retained while `xuan_four_zhu_card` owns the concrete
+/// pillar style config model.
+export 'package:xuan_four_zhu_card/features/four_zhu_card/widgets/editable_fourzhu_card/models/pillar_style_config.dart';
